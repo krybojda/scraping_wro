9349c8a3b6149311c38a0034d145fe65460c2850
Krystian Bojda <123501489+krybojda@users.noreply.github.com>
2026-02-07 22:13:26 +0100

    modified:   run_vps.sh

--- run_vps.sh
+++ run_vps.sh
@@ -14,7 +14,7 @@ git pull --rebase origin main >> scraper.log 2>&1
 # To polecenie zablokuje skrypt na 6 godzin, dopóki kontener nie skończy pracy
 # >> oznacza "dopisz na końcu pliku"
 # 2>&1 oznacza "zapisz też błędy (stderr) w tym samym miejscu co zwykłe napisy"
-docker-compose up --build >> scraper_output.log 2>&1
+docker compose up --build >> scraper_output.log 2>&1
 
 # 4. Wyślij wyniki (mieszkania_vps.csv) do repozytorium
 git add mieszkania_vps.csv >> scraper.log 2>&1
